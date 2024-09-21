7f544c1a58f25aaf2572b67230ae8ad4d75482ba
Dnfreddie <defnotfreddie@gmai.com>
2024-09-21 21:00:49 +0200

    Git term interace and new test for ContainsPattern

---
+++ lib/lib_test.go
@@ -0,0 +1,43 @@
+package lib
+
+import (
+	"testing"
+
+)
+
+type testCase struct {
+	name      string
+	line      string
+	pattern   Pattern
+	shouldErr bool
+	message   string
+}
+
+func TestContainsPattern(t *testing.T) {
+	testCases := []testCase{
+		{"Test TODO", "TODO!! This is an urgent task", TODO, false, "! This is an urgent task"},
+		{"Test IDEA", "IDEA: This is an Idea", IDEA, false, ": This is an Idea"},
+		{"Test NOTE", "NOTE! This is the Note!", NOTE, false, "This is the Note!"},
+		{"Test no pattern", "Random text without patterns", TODO, true, ""},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := ContainsPattern(tc.line, 1, tc.pattern)
+
+			if tc.shouldErr {
+				if result != nil {
+					t.Errorf("Expected result to be nil, but got: %v", result)
+				}
+			} else {
+				if result == nil {
+					t.Errorf("Expected a valid result, but got nil")
+				} else if result.Title != tc.message {
+					t.Errorf("Expected result title to be '%s', but got '%s'", tc.message, result.Title)
+				}
+			}
+		})
+	}
+}
+
+
--- lib/terminal.go
+++ lib/terminal.go
@@ -3,31 +3,39 @@ package lib
 import (
 	"fmt"
 	"os"
-	"os/signal"
 	"sort"
 	"strings"
-	"syscall"
+
 	"golang.org/x/term"
 )
 
-type EscapeCode string
-type Signal int
-type Color string
 
+
+type  Key int
+const (
+	Unknown  = iota
+	CtrlC
+	Backspace
+	Enter
+	Escape
+	UpArrow
+	DownArrow
+	Other
+)
+
+type EscapeCode string
 const (
 	// Escape codes
-	Clear        EscapeCode = "\033[H\033[2J\033[H" // Clear screen and reset cursor
-	ResetCursor  EscapeCode = "\033[0G"             // Move cursor to the beginning of the line
-	HideCursor   EscapeCode = "\033[?25l"
-	ShowCursor   EscapeCode = "\033[?25h"
-
-	// Signals
-	CtrlC     Signal = 3
-	Backspace Signal = 127
-	Enter     Signal = 13
-	Escape    Signal = 27
-
-	// Colors
+	Clear       EscapeCode = "\033[H\033[2J\033[H" // Clear screen and reset cursor
+	ResetCursor EscapeCode = "\033[0G"             // Move cursor to the beginning of the line
+	HideCursor  EscapeCode = "\033[?25l"
+	ShowCursor  EscapeCode = "\033[?25h")
+	
+func clearTerminal() {
+	print(Clear)
+}
+type Color string
+const (// Colors
 	Red    Color = "\033[31m"
 	Reset  Color = "\033[0m"
 	Green  Color = "\033[32m"
@@ -40,8 +48,91 @@ func InColors(c Color, s string) {
 	fmt.Print(c, s, Reset)
 }
 
-func clearTerminal() {
-	fmt.Print(Clear)
+type Term interface {
+	Start()
+	Close()
+	Clear()
+}
+
+func Quit(t Term) {
+	t.Close()
+	t.Clear()
+	defer os.Exit(0)
+}
+
+func NewTerm() Term {
+	newTerm := &Terminal{}
+	newTerm.Start()
+	return newTerm
+}
+
+
+type Terminal struct {
+	oldState *term.State
+}
+
+func (t *Terminal) Start() {
+	t.startRawMode()
+}
+
+func (t *Terminal) startRawMode() {
+	var err error
+	t.oldState, err = term.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
+		panic(fmt.Sprintf("Failed to set raw mode: %v", err))
+	}
+	fmt.Print(HideCursor)
+}
+
+func (t *Terminal) Close() {
+	fmt.Print(ShowCursor)
+	t.stopRawMode()
+}
+
+func (t *Terminal) stopRawMode() {
+	if t.oldState != nil {
+		if err := term.Restore(int(os.Stdin.Fd()), t.oldState); err != nil {
+			panic(fmt.Sprintf("Failed to restore terminal: %v", err))
+		}
+		t.oldState = nil
+	}
+}
+
+func (t *Terminal) Clear() {
+	clearTerminal()
+}
+
+
+func Read() (Key, rune) {
+	buf := make([]byte, 3)
+	n, err := os.Stdin.Read(buf)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to read input: %v", err))
+	}
+	if n == 0 {
+		return Unknown, 0
+	}
+
+	switch buf[0] {
+	case 3:
+		return CtrlC, 0
+	case 127:
+		return Backspace, 0
+	case 13:
+		return Enter, 0
+	case 27:
+		if n > 1 && buf[1] == '[' {
+			switch buf[2] {
+			case 'A':
+				return UpArrow, 0
+			case 'B':
+				return DownArrow, 0
+			}
+		}
+		return Escape, 0
+	default:
+		return Other, rune(buf[0])
+	}
 }
 
 func RunTerm[T any](maps []map[string]T) (map[string]T, error) {
@@ -59,83 +150,44 @@ func RunTerm[T any](maps []map[string]T) (map[string]T, error) {
 		return nil, fmt.Errorf("No items available.")
 	}
 
-	sigs := make(chan os.Signal, 1)
-	signal.Notify(sigs, syscall.SIGTERM, syscall.SIGINT)
-
-	// Save terminal state
-	oldState, err := term.GetState(int(os.Stdin.Fd()))
-	if err != nil {
-		fmt.Println("Error getting terminal state:", err)
-		return nil, err
-	}
-
-	_, err = term.MakeRaw(int(os.Stdin.Fd()))
-	if err != nil {
-		fmt.Println("Error setting raw mode:", err)
-		return nil, err
-	}
-	defer term.Restore(int(os.Stdin.Fd()), oldState)
-
-	go func() {
-		<-sigs
-		term.Restore(int(os.Stdin.Fd()), oldState)
-		os.Exit(1)
-	}()
-
-	buf := make([]byte, 3) 
+	term := NewTerm()
+	defer term.Close()
 
 	for {
-		clearTerminal()
-		fmt.Print("> ", input, "\n\n")
-		fmt.Print(HideCursor)
-		fmt.Print(ResetCursor)
+		term.Clear()
+		fmt.Printf("> %s\n\n", input)
 
 		filteredItems := filterItems(combinedItems, input)
-
 		displayResults(filteredItems, selectionIndex)
 
-		fmt.Print(ResetCursor)
+		key, r := Read()
 
-		n, err := os.Stdin.Read(buf)
-		if err != nil {
-			fmt.Println("Error reading input:", err)
-			return nil, err
-		}
-
-		if n > 0 {
-			switch buf[0] {
-			case byte(CtrlC):
-				fmt.Print(Clear)
-				term.Restore(int(os.Stdin.Fd()), oldState)
-				fmt.Print(ShowCursor)
-				os.Exit(0)
-			case byte(Backspace):
-				if len(input) > 0 {
-					input = input[:len(input)-1]
-				}
-			case byte(Enter):
-				term.Restore(int(os.Stdin.Fd()), oldState)
-				clearTerminal()
-				if len(filteredItems) > 0 {
-					selectedKey := filteredItems[selectionIndex]
-					return map[string]T{selectedKey: combinedItems[selectedKey]}, nil
-				}
-			case byte(Escape):
-				if n > 1 && buf[1] == '[' {
-					switch buf[2] {
-					case 'A': // Up arrow
-						if selectionIndex > 0 {
-							selectionIndex--
-						}
-					case 'B': // Down arrow
-						if selectionIndex < len(filteredItems)-1 {
-							selectionIndex++
-						}
-					}
-				}
-			default:
-				input += string(buf[0])
+		switch key {
+		case CtrlC:
+		Quit(term)
+		
+		case Backspace:
+			if len(input) > 0 {
+				input = input[:len(input)-1]
 			}
+		case Enter:
+			if len(filteredItems) > 0 {
+				selected := filteredItems[selectionIndex]
+				return map[string]T{selected: combinedItems[selected]}, nil
+			}
+		case Escape:
+		Quit(term)
+			
+		case UpArrow:
+			if selectionIndex > 0 {
+				selectionIndex--
+			}
+		case DownArrow:
+			if selectionIndex < len(filteredItems)-1 {
+				selectionIndex++
+			}
+		case Other:
+			input += string(r)
 		}
 	}
 }
@@ -144,6 +196,7 @@ func displayResults(filteredItems []string, selectionIndex int) {
 	if len(filteredItems) == 0 {
 		fmt.Println("No results found.")
 	} else {
+		fmt.Print(ResetCursor)
 		for i, item := range filteredItems {
 			if i == selectionIndex {
 				InColors(Blue, fmt.Sprintf("> %v\n", item))
