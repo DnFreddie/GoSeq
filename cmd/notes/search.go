998bf3acee9011be8538daaba2c4d0f0be4bdeb7
Dnfreddie <defnotfreddie@gmai.com>
2024-09-23 18:34:13 +0200

    Search command description

--- cmd/notes/search.go
+++ cmd/notes/search.go
@@ -16,13 +16,9 @@ var iname bool
 var regex bool
 var SearchCmd = &cobra.Command{
 	Use:   "search",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Search for a pattern in the notes and open the ones that are relevant.",
+	Long: `Accept the pattern along with the grep flags -i or -E to search for matches.
+It will then display the matches and allow you to open the desired note.`,
 	Run: func(cmd *cobra.Command, args []string) {
 
 		var re lib.GrepFlag
