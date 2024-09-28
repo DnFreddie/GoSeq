d6a20d0c52e61ac5cccc73eda60b37ef5599fdc7
Dnfreddie <defnotfreddie@gmai.com>
2024-09-29 00:38:18 +0200

    More consise Todo function

--- pkg/todo/todo.go
+++ pkg/todo/todo.go
@@ -1,90 +1,77 @@
 package todo
 
-
 import "strings"
 
 type Pattern uint
 
 const (
-	NONE Pattern = 0
-	TODO Pattern = 1 << iota
-	IDEA
-	NOTE
-	ALL Pattern = TODO | IDEA | NOTE
+    NONE Pattern = 0
+    TODO Pattern = 1 << iota
+    IDEA
+    NOTE
+    ALL Pattern = TODO | IDEA | NOTE
 )
 
 type Todo struct {
-	Keyword       string
-	Urgency       int
-	ID            *string
-	Filename      string
-	Line          int
-	BodySeparator string
-	Title         string `json:"title"`
-	Body          string `json:"body"`
-	Pattern       Pattern
+    Keyword       string
+    Urgency       int
+    ID            *string
+    Filename      string
+    Line          int
+    BodySeparator string
+    Title         string `json:"title"`
+    Body          string `json:"body"`
+    Pattern       Pattern
+}
+
+var patternKeywords = map[Pattern]string{
+    TODO: "TODO",
+    IDEA: "IDEA",
+    NOTE: "NOTE",
 }
 
 func ContainsPattern(line string, lineIndex int, patterns Pattern) *Todo {
-
-	if patterns&TODO != 0 {
-		index := strings.Index(line, "TODO")
-		if index != -1 {
-			return processMatch(line, lineIndex, index, "TODO", TODO)
-		}
-	}
-
-	if patterns&IDEA != 0 {
-		index := strings.Index(line, "IDEA")
-		if index != -1 {
-			return processMatch(line, lineIndex, index, "IDEA", IDEA)
-		}
-	}
-
-	if patterns&NOTE != 0 {
-		index := strings.Index(line, "NOTE")
-		if index != -1 {
-			return processMatch(line, lineIndex, index, "NOTE", NOTE)
-		}
-	}
-
-	return nil
+    for pattern, keyword := range patternKeywords {
+        if patterns&pattern != 0 {
+            if index := strings.Index(line, keyword); index != -1 {
+                return processMatch(line, lineIndex, index, keyword, pattern)
+            }
+        }
+    }
+    return nil
 }
 
 func processMatch(line string, lineIndex int, index int, keyword string, pattern Pattern) *Todo {
-	var titleIndex int
-	var urgency int
+    var titleIndex, urgency int
+    lastChar := keyword[len(keyword)-1]
+    startIndex := index + len(keyword)
 
-	lastChar := keyword[len(keyword)-1]
+    for i := startIndex; i < len(line); i++ {
+        if line[i] == lastChar {
+            urgency++
+        } else if line[i] == '!' || line[i] != ' ' {
+            titleIndex = i
+            if line[i] == '!' {
+                titleIndex++
+            }
+            break
+        }
+    }
 
-	startIndex := index + len(keyword)
+    if titleIndex == 0 {
+        titleIndex = startIndex
+    }
 
-	for i := startIndex; i < len(line); i++ {
-		if line[i] == lastChar {
-			urgency += 1
-		} else if line[i] == '!' {
-			titleIndex = i + 1
-			break
-		} else if line[i] != ' ' {
-			titleIndex = i
-			break
-		}
-	}
+    title := strings.TrimSpace(line[titleIndex:])
+    if title == "" {
+        return nil
+    }
 
-	if titleIndex == 0 {
-		titleIndex = startIndex
-	}
-
-	title := strings.TrimSpace(line[titleIndex:])
-	if title == "" {
-		return nil
-	}
-
-	return &Todo{
-		Keyword: keyword,
-		Urgency: urgency,
-		Title:   title,
-		Line:    lineIndex,
-		Pattern: pattern,
-	}
+    return &Todo{
+        Keyword: keyword,
+        Urgency: urgency,
+        Title:   title,
+        Line:    lineIndex,
+        Pattern: pattern,
+    }
 }
